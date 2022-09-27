2fd0cdd8fd8ec2611e298bd0f74e7ecefa458ae8
charltin <2268588240@qq.com>
2022-09-27 15:47:46 +0800

    add: 字符串的拼接

--- 11-字符串-截取-拆分-拼接.py
+++ 11-字符串-截取-拆分-拼接.py
@@ -33,21 +33,54 @@ print(x, 'rpartition:', x.rpartition(' '))
 # split/rsplit(sep=None, maxsplit(-1)) 默认分隔符为空字符串, 匹配全部分隔符
 print(x, 'split:', x.split())
 print(x, 'rsplit:', x.rsplit())
-print(x, 'rsplit:', x.rsplit(' ', 1))
+print(x, 'rsplit-1:', x.rsplit(' ', 1))
+print()
 
 # 2.3 splitlines(keepends=False) 根据行分割
 x = 'Welcome\nto\r\nwonderland'
-print(x)
+print('原字符串: ', x)
 print('splitlines:', x.splitlines())
-print('splitlines:', x.splitlines(True))  # 包括换行符
+print('splitlines-True:', x.splitlines(True))  # 包括换行符
+print()
 
 # 3. 拼接
 list_join = ['welcome', 'to', 'wonderland']
 tuple_join = ('Forest', 'cottages', 'castles', 'cards that can talk')
 
 # 3.1 join
-print(' '.join(list_join))
-print(' and '.join(tuple_join))
+print('list_join_space: ', ' '.join(list_join))
+print('tuple_join_and: ', ' and '.join(tuple_join))
 
 # 拼接两个字符串 'welcome' -> 'welcomewelcome' 用join拼接的效率比用加法拼接高
 ''.join(['welcome', 'welcome'])
+print()
+
+# 3.2 format 格式化函数
+format_1 = 'If this was a {} then at least I\'ve got'.format('dream')
+print('format的基本语法: ', format_1)
+
+format_2 = 'Welcome to {}. We\'ve got it {}'.format('wonderland', 'all')
+print('format接收多个参数: ', format_2)
+
+format_3 = '{0} and {1}, {2} and {3} that can talk'.format(
+    'Forest', 'cottages', 'castles', 'cards')
+format_4 = '{1} and {0}, {3} and {2} that can talk'.format(
+    'Forest', 'cottages', 'castles', 'cards')
+print('format设定指定位置(按原顺序): ', format_3)
+print('format设定指定位置(指定顺序): ', format_4)
+
+format_5 = 'Me llamo {name}, {age} anos'.format(name='Bayes', age='ventidos')
+print('format关键字索引: ', format_5)
+print()
+
+# 3.3 f-string
+name = 'Bayes'
+age = 'ventidos'
+
+intro = f'Me llamo {name}, {age} anos'
+print('f-string: ', intro)
+print()
+
+# 3.4 来自C语言的%方式
+print('C语言的%方式: ', '%s %s' % ('Hello', 'World'))
+print()
